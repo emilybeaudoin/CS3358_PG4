465e803de934a408c6dc1de04920fb3ec7bcc0d5
Emily Beaudoin <erb64@txstate.edu>
2017-03-01 16:25:15 -0600

    Update 3358_2_Beaudoin_Emily_PG4.cpp

--- 3358_2_Beaudoin_Emily_PG4.cpp
+++ 3358_2_Beaudoin_Emily_PG4.cpp
@@ -18,52 +18,83 @@ displayListBackwards();
 splitList();
 deleteList();
 
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+struct ListNode //node structure
+{
+	int number; //data
+	list *next; //pointer to next node
+}
+
+appendNode(ListNode *, int);
+insertNode(ListNode *, int); //inserts a node at the beginning
+insertNode(ListNode *, int,int); //inserts a node containing a position(starting at 0)
+deleteNode(ListNode *, int); //deletes a node with contents
+deleteNodeAt(ListNode *, int); //deletes a node at position 
+displayListForward(ListNode *); 
+displayListBackwards(ListNode *);
+splitList(ListNode *);
+deleteList(ListNode *);
+
 int main ()
 {
+	ListNode *head = NULL; //initializing the main empty list
+	int SIZE_OF_MAIN = 19;
+
+	//build a sorted list of 12 random integers between 10 and 20
+	insertNode(head, -100);
+	insertNode(head, 15,2); //inserts 15 at 3rd position in list
+	appendNode(head, 1000);
+	deleteNode(head, 5);
+	deleteNodeAt(head, 1); //removes 2nd node from the list
+
+
 
 }
 
-appendNode(int num)
+appendNode(ListNode *h, int num)
 {
 
 }
 
-insertNode(int num)
+insertNode(ListNode *h, int num)
 {
 
 }
 
-insertNode(int num, int position)
+insertNode(ListNode *h, int num, int position)
 {
 
 }
 
-deleteNode(int num)
+deleteNode(ListNode *h, int num)
 {
 
 }
 
-deleteNodeAt(int num)
+deleteNodeAt(ListNode *h, int num)
 {
 
 }
 
-displayListForward()
+displayListForward(ListNode *h)
 {
 
 } 
 
-displayListBackwards()
+displayListBackwards(ListNode *h)
 {
 
 }
 
-splitlist()
+splitlist(ListNode *h)
 {
 
 }
 
-deleteList()
+deleteList(ListNode *h)
 {
 	
 }
