8cace25206ab5164b508ef41f25242d676d74fe1
Emily Beaudoin <erb64@txstate.edu>
2017-03-01 15:24:13 -0600

    Add files via upload

---
+++ 3358_2_Beaudoin_Emily_PG4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+struct ListNode //node structure
+{
+	int number; //data
+	list *next; //pointer to next node
+}
+
+appendNode(int num)
+{
+
+}
+
+insertNode(int num)
+{
+
+}
+
+deleteNode(int num)
+{
+
+}
+
+int main ()
+{
+
+}
